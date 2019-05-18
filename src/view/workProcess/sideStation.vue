767d798c8c00e29bd658340d08e0db819f204903
zhangzhengqiqi <1574900601@qq.com>
2019-05-18 13:14:00 +0800

    旁站

--- src/view/workProcess/sideStation.vue
+++ src/view/workProcess/sideStation.vue
@@ -20,13 +20,15 @@
         <template slot-scope="{ row }" slot="name">
             <strong>{{ row.name }}</strong>
         </template>
-        <!-- <template slot="action" slot-scope="{ row, index }">
+        <template slot="action" slot-scope="{ row, index }">
+            <Button type="success" size="small" @click="showFlows(row,index)">流程图</Button>
             <Button type="primary" size="small" style="margin-right: 1px"
             v-show="(currentRank < row.originRank && row.isSubmit === 1) || (currentEmplId === row.sponsor.id && row.isSubmit === 0)" @click="edit(row, index)">编辑</Button>
             <Button type="error" size="small"
             v-show="(currentRank < row.originRank && row.isSubmit === 1) || (currentEmplId === row.sponsor.id && row.isSubmit === 0)" @click="remove(row, index)">删除</Button>
-            <Button type="success" size="small" @click="showFlows(row,index)">流程图</Button>
-        </template> -->
+            <Button type="warning" size="small"
+            v-show="(currentRank < row.originRank && row.isSubmit === 1)" @click="editCheckStatus(row,index)">审核</Button>
+        </template>
         </Table>
         <div style="margin: 10px;overflow: hidden">
             <div style="float: right;">
@@ -43,12 +45,104 @@
                 ></Page>
             </div>
         </div>
+        <Modal
+        v-model="showAddModal"
+        title="新增 旁站">
+            <Form ref="formData" :model="formData" :rules="ruleValidate" :label-width="100">
+                <FormItem label="部位" prop="part">
+                <Input v-model="formData.part" placeholder="部位" />
+                </FormItem>
+                <FormItem label="所属项目" prop="project">
+                <Select v-model="formData.project">
+                    <Option v-for="item in projectList" :value="item" :key="item.id" :label="item.name"></Option>
+                </Select>
+                </FormItem>
+                <FormItem label="简要内容" prop="brief">
+                <Input v-model="formData.brief" placeholder="简要内容" />
+                </FormItem>
+                <FormItem label="解决问题" prop="problems">
+                <Input v-model="formData.problems" placeholder="解决问题" />
+                </FormItem>
+                <FormItem label="旁站日期" prop="sideStationDate">
+                <DatePicker v-model="formData.sideStationDate" type="date" placeholder="Select date" style="width: 200px"></DatePicker>
+                </FormItem>
+            </Form>
+            <div slot="footer">
+                <Button type="primary" @click="confirmAdd('formData', 0)">保存</Button>
+                <Button type="primary" @click="confirmAdd('formData', 1)">提交</Button>
+                <Button @click="cancelAdd('formData')" style="margin-left: 8px">取消</Button>
+            </div>
+        </Modal>
+        <Modal
+        v-model="showEditModal"
+        title="编辑 旁站">
+            <Form ref="formData" :model="formData" :rules="ruleValidate" :label-width="100">
+                <FormItem label="部位" prop="part">
+                <Input v-model="formData.part" placeholder="部位" />
+                </FormItem>
+                <FormItem label="简要内容" prop="brief">
+                <Input v-model="formData.brief" placeholder="简要内容" />
+                </FormItem>
+                <FormItem label="解决问题" prop="problems">
+                <Input v-model="formData.problems" placeholder="解决问题" />
+                </FormItem>
+            </Form>
+            <div slot="footer">
+                <Button type="primary" @click="confirmEdit('formData', 0)">保存</Button>
+                <Button type="primary" @click="confirmEdit('formData', 1)">提交</Button>
+                <Button @click="cancelEdit('formData')" style="margin-left: 8px">取消</Button>
+            </div>
+        </Modal>
+        <Modal
+        v-model="showDeleteModal"
+        width="360">
+            <p slot="header" style="color:#f60;text-align:center">
+                <Icon type="ios-information-circle"></Icon>
+                <span>删除提醒</span>
+            </p>
+            <div style="text-align:center">
+                <p>删除后将不可恢复</p>
+                <p>您确认删除?</p>
+            </div>
+            <div slot="footer">
+                <Button type="error" @click="confirmDelete()">删除</Button>
+                <Button @click="cancelDelete()" style="margin-left: 8px">取消</Button>
+            </div>
+        </Modal>
+        <Modal
+        v-model="showCheckStatusModal"
+        width="360">
+            <p slot="header" style="color:#f60;text-align:center">
+                <Icon type="ios-information-circle"></Icon>
+                <span>审核</span>
+            </p>
+            <div style="text-align:center">
+                <Select v-model="formData.checkStatus" :placeholder="currentStatus">
+                    <Option v-for="item in checkStatusList" :value="item" :key="item.id" :label="item.status"></Option>
+                </Select>
+            </div>
+            <div slot="footer">
+                <Button type="error" @click="confirmCheckStatus()">确认</Button>
+                <Button @click="cancelCheckStatus()" style="margin-left: 8px">取消</Button>
+            </div>
+        </Modal>
     </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      currentStatus: '',
+      projectList: [],
+      checkStatusList: [],
+      currentRowId: '',
+      currentEmplId: 0,
+      // 模块显示
+      showAddModal: false,
+      showEditModal: false,
+      showDeleteModal: false,
+      showCheckStatusModal: false,
+      currentRank: '',
       // 分页
       pageSize: 5,
       pageNo: 0,
@@ -63,17 +157,17 @@ export default {
         {
           title: 'ID',
           key: 'id',
-          width: 100
+          width: 60
         },
         {
           title: '部位',
-          key: 'partName',
-          width: 110
+          key: 'part',
+          width: 70
         },
         {
           title: '所属项目',
           key: 'project',
-          width: 140,
+          width: 120,
           render: (h, params) => {
             let projec = params.row.project
             return h('span', projec.name)
@@ -82,24 +176,61 @@ export default {
         {
           title: '创建人',
           key: 'sponsor',
-          width: 150,
+          width: 130,
           render: (h, params) => {
             let employee = params.row.sponsor
-            return h('span', employee.username)
+            return h('span', employee.name)
           }
         },
         {
           title: '创建日期',
           key: 'createDate',
-          width: 200
+          width: 180,
+          render: (h, params) => {
+            const createDate = params.row.createDate
+            if (createDate === null || createDate === '') {
+              return h('span', '')
+            } else {
+              return h('span', createDate.substring(0, 10))
+            }
+          }
         },
         {
-          title: '状态',
-          key: 'status',
+          title: '旁站日期',
+          key: 'sideStationDate',
+          width: 180,
+          render: (h, params) => {
+            const sideStationDate = params.row.sideStationDate
+            if (sideStationDate === null || sideStationDate === '') {
+              return h('span', '')
+            } else {
+              return h('span', sideStationDate.substring(0, 10))
+            }
+          }
+        },
+        {
+          title: '审核状态',
+          key: 'checkStatus',
           width: 150,
           render: (h, params) => {
-            let newStatus = params.row.status
-            return h('span', newStatus.name)
+            let newStatus = params.row.checkStatus
+            return h('span', newStatus.status)
+          }
+        },
+        {
+          title: '提交状态',
+          key: 'isSubmit',
+          width: 140,
+          render: (h, params) => {
+            const Row = params.row
+            const color = Row.isSubmit === 0 ? 'default' : 'success'
+            const text = Row.isSubmit === 0 ? '未提交' : '已提交'
+            return h('Tag', {
+              props: {
+                type: 'dot',
+                color: color
+              }
+            }, text)
           }
         },
         {
@@ -108,24 +239,184 @@ export default {
           width: 223,
           fixed: 'right'
         }
-      ]
+      ],
+      // 校验规则
+      ruleValidate: {
+        part: [
+          {required: true, message: '不可以为空', trigger: 'blur'}
+        ],
+        brief: [
+          {required: true, message: '不可以为空', trigger: 'blur'}
+        ],
+        problems: [
+          {required: true, message: '不可以为空', trigger: 'blur'}
+        ]
+      }
     }
   },
+  created () {
+    this.getProjectsList()
+    this.getSideStationDataList()
+    const departmentPosition = JSON.parse(localStorage.getItem('currentUser')).departmentPosition
+    this.currentRank = departmentPosition.rank
+    this.currentEmplId = JSON.parse(localStorage.getItem('currentUser')).id
+  },
   methods: {
+    add () {
+      this.formData = {}
+      this.showAddModal = true
+    },
+    confirmAdd (name, isSubmit) {
+      if (this.formData.project === undefined || this.formData.project === null) {
+        this.$Message.error('请选择所属项目！')
+      } else {
+        this.formData.isSubmit = isSubmit
+        this.$refs[name].validate((valid) => {
+          if (valid) {
+            let url = '/sideStation/saveSideStation?access_token=' + localStorage.getItem('jwtToken')
+            this.$http.postForm(url, JSON.stringify(this.formData)).then(res => {
+              this.showAddModal = false
+              if (res.data.status === true) {
+                this.getSideStationDataList()
+                this.$Message.success(res.data.message)
+              } else {
+                this.$Message.error(res.data.message)
+              }
+            })
+          } else {
+            this.$Message.error('表单数据校验失败!')
+          }
+        })
+      }
+    },
+    cancelAdd () {
+      this.showAddModal = false
+      this.$Message.info('您已取消添加！')
+    },
+    edit (row, index) {
+      this.formData = row
+      this.showEditModal = true
+    },
+    confirmEdit (name, isSubmit) {
+      if (this.formData.project === undefined || this.formData.project === null) {
+        this.$Message.error('请选择所属项目！')
+      } else {
+        this.formData.isSubmit = isSubmit
+        this.$refs[name].validate((valid) => {
+          if (valid) {
+            let url = '/sideStation/saveSideStation?access_token=' + localStorage.getItem('jwtToken')
+            this.$http.postForm(url, JSON.stringify(this.formData)).then(res => {
+              this.showEditModal = false
+              if (res.data.status === true) {
+                this.getSideStationDataList()
+                this.$Message.success(res.data.message)
+              } else {
+                this.$Message.error(res.data.message)
+              }
+            })
+          } else {
+            this.$Message.error('表单数据校验失败!')
+          }
+        })
+      }
+    },
+    cancelEdit () {
+      this.showEditModal = false
+      this.$Message.info('您已取消修改！')
+    },
+    remove (row, index) {
+      this.showDeleteModal = true
+      this.currentRowId = row.id
+    },
+    confirmDelete () {
+      let data = {
+        access_token: localStorage.getItem('jwtToken'),
+        id: this.currentRowId
+      }
+      let url = '/sideStation/deleteSideStationById'
+      this.$http.post(url, data).then(res => {
+        this.showDeleteModal = false
+        if (res.data.status === true) {
+          this.$Message.success(res.data.message)
+          this.getSideStationDataList()
+        } else {
+          this.$Message.error(res.data.message)
+        }
+      })
+    },
+    cancelDelete () {
+      this.showDeleteModal = false
+      this.$Message.info('您已取消删除！')
+    },
+    showFlows (row, index) {
+    },
+    getCheckStatusList () {
+      let data = {
+        access_token: localStorage.getItem('jwtToken')
+      }
+      let url = '/checkStatus/listAllCheckStatus'
+      this.$http.post(url, data).then(res => {
+        if (res.status === 200) {
+          this.checkStatusList = res.data
+        }
+      })
+    },
+    editCheckStatus (row, index) {
+      this.formData = row
+      this.currentStatus = row.checkStatus.status
+      this.getCheckStatusList()
+      this.showCheckStatusModal = true
+    },
+    confirmCheckStatus () {
+      let url = '/sideStation/saveSideStation?access_token=' + localStorage.getItem('jwtToken')
+      this.$http.post(url, JSON.stringify(this.formData)).then(res => {
+        this.showCheckStatusModal = false
+        if (res.data.status === true) {
+          this.$Message.info('审核成功')
+          this.getSideStationDataList()
+        } else {
+          this.$Message.info('审核失败')
+        }
+      })
+    },
+    cancelCheckStatus (row, index) {
+      this.formData.checkStatus = this.currentStatus
+      this.showCheckStatusModal = false
+      this.$Message.info('取消审核')
+    },
+    search () {
+      this.getSideStationDataList()
+    },
+    getProjectsList () {
+      let data = {
+        access_token: localStorage.getItem('jwtToken')
+      }
+      let url = '/project/listAllProjects'
+      this.$http.post(url, data).then(res => {
+        if (res.status === 200) {
+          this.projectList = res.data
+        }
+      })
+    },
     getSideStationDataList () {
       let data = {
         access_token: localStorage.getItem('jwtToken'),
-        pageNo: this.pageNo,
-        pageSize: this.pageSize,
         partName: this.partName
       }
-      let url = '/sideStation/listSideStation'
+      let url = '/sideStation/listSideStation/pageNo/' + this.pageNo + '/pageSize/' + this.pageSize
       this.$http.post(url, data).then(res => {
         if (res.status === 200) {
           this.sideStationData = res.data.content
           this.dataTotal = res.data.totalElements
         }
       })
+    },
+    // 分页
+    changePageNo (pageNo) {
+      this.pageNo = pageNo - 1
+    },
+    changePageSize (pageSize) {
+      this.pageSize = pageSize
     }
   },
   watch: {
