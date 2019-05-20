0d9a37df08155ddba60682bac28c0a4fb80ff5cc
zhangzhengqiqi <1574900601@qq.com>
2019-05-20 20:42:43 +0800

    平行检验

--- src/view/workProcess/parallelTest.vue
+++ src/view/workProcess/parallelTest.vue
@@ -27,7 +27,7 @@
             <Button type="error" size="small"
             v-show="(currentRank < row.originRank && row.isSubmit === 1) || (currentEmplId === row.sponsor.id && row.isSubmit === 0)" @click="remove(row, index)">删除</Button>
             <Button type="warning" size="small"
-            v-show="(currentRank < row.originRank && row.isSubmit === 1)" @click="editCheckStatus(row,index)">审核</Button>
+            v-show="showCheck(row)" @click="check(row)">审核</Button>
         </template>
         </Table>
         <div style="margin: 10px;overflow: hidden">
@@ -74,6 +74,11 @@
         v-model="showEditModal"
         title="编辑 平行验收">
             <Form ref="formData" :model="formData" :rules="ruleValidate" :label-width="100">
+                <FormItem label="所属项目" prop="project">
+                <Select v-model="formData.project" :placeholder="currentProject">
+                    <Option v-for="item in projectList" :value="item" :key="item.id" :label="item.name"></Option>
+                </Select>
+                </FormItem>
                 <FormItem label="部位" prop="part">
                 <Input v-model="formData.part" placeholder="部位" />
                 </FormItem>
@@ -104,38 +109,44 @@
             </div>
         </Modal>
         <Modal
-        v-model="showCheckStatusModal"
-        width="360">
-            <p slot="header" style="color:#f60;text-align:center">
-                <Icon type="ios-information-circle"></Icon>
-                <span>审核</span>
-            </p>
-            <div style="text-align:center">
-                <Select v-model="formData.checkStatus" :placeholder="currentStatus">
-                    <Option v-for="item in checkStatusList" :value="item" :key="item.id" :label="item.status"></Option>
-                </Select>
-            </div>
-            <div slot="footer">
-                <Button type="error" @click="confirmCheckStatus()">确认</Button>
-                <Button @click="cancelCheckStatus()" style="margin-left: 8px">取消</Button>
-            </div>
-        </Modal>
+        v-model="showCheckResultModal"
+        title="审核">
+        <Form ref="formData" :label-width="100">
+          <FormItem label="审核结果" prop="result">
+            <Select v-model="checkResult">
+              <Option value=true>通过</Option>
+              <Option value=false>不通过</Option>
+            </Select>
+          </FormItem>
+          <FormItem label="审核意见" prop="message">
+            <Input v-model="checkMsg" placeholder="审核意见" />
+          </FormItem>
+        </Form>
+        <div slot="footer">
+          <Button type="primary" @click="confirmCheck()">提交</Button>
+          <Button @click="cancelCheck()" style="margin-left: 8px">取消</Button>
+        </div>
+      </Modal>
     </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      currentProject: '',
       currentStatus: '',
       projectList: [],
       checkStatusList: [],
       currentRowId: '',
+      taskId: '',
+      checkResult: '',
+      checkMsg: '',
       currentEmplId: 0,
       // 模块显示
       showAddModal: false,
       showEditModal: false,
       showDeleteModal: false,
-      showCheckStatusModal: false,
+      showCheckResultModal: false,
       currentRank: 0,
       // 分页
       pageSize: 5,
@@ -183,7 +194,7 @@ export default {
         {
           title: '检查日期',
           key: 'testDate',
-          width: 180,
+          width: 160,
           render: (h, params) => {
             const createDate = params.row.testDate
             if (createDate === null || createDate === '') {
@@ -196,15 +207,20 @@ export default {
         {
           title: '简要内容',
           key: 'brief',
-          width: 180
+          width: 150
         },
         {
           title: '审核状态',
-          key: 'checkStatus',
-          width: 150,
+          key: 'auditStatus',
+          width: 120
+        },
+        {
+          title: '审核人',
+          key: 'employeeAudit',
+          width: 80,
           render: (h, params) => {
-            let newStatus = params.row.checkStatus
-            return h('span', newStatus.status)
+            let employee = params.row.majorAudit.employee.name
+            return h('span', employee)
           }
         },
         {
@@ -249,6 +265,12 @@ export default {
     this.currentEmplId = JSON.parse(localStorage.getItem('currentUser')).id
   },
   methods: {
+    // 判断是否显示 审核 按钮，以及“总监审核”按钮
+    showCheck (row) {
+      if (row.needAudit) {
+        return true
+      }
+    },
     add () {
       this.formData = {}
       this.showAddModal = true
@@ -285,6 +307,7 @@ export default {
       this.showEditModal = true
       this.editData.part = row.part
       this.editData.brief = row.brief
+      this.currentProject = row.project.name
     },
     confirmEdit (name, isSubmit) {
       if (this.formData.project === undefined || this.formData.project === null) {
@@ -352,28 +375,41 @@ export default {
         }
       })
     },
-    editCheckStatus (row, index) {
-      this.formData = row
-      this.currentStatus = row.checkStatus.status
-      this.getCheckStatusList()
-      this.showCheckStatusModal = true
+    // 弹出审核弹框
+    check (row) {
+      this.showCheckResultModal = true
+      this.currentRowId = row.id
+      this.taskId = row.taskId
+      alert(this.taskId)
     },
-    confirmCheckStatus () {
-      let url = '/parallelTest/saveParallelTest?access_token=' + localStorage.getItem('jwtToken')
-      this.$http.post(url, JSON.stringify(this.formData)).then(res => {
-        this.showCheckStatusModal = false
+    // 提交审核结果
+    confirmCheck () {
+      let url = '/parallelTest/checkParallelTest'
+      let data = {
+        access_token: localStorage.getItem('jwtToken'),
+        parallelTestId: this.currentRowId,
+        taskId: this.taskId,
+        approved: this.checkResult,
+        auditOpinion: this.checkMsg
+      }
+      this.$http.post(url, data).then(res => {
+        this.showCheckResultModal = false
         if (res.data.status === true) {
-          this.$Message.info('审核成功')
           this.getParallelTestDataList()
+          this.$Message.success(res.data.message)
         } else {
-          this.$Message.info('审核失败')
+          this.$Message.error(res.data.message)
         }
       })
+      this.checkResult = ''
+      this.checkMsg = ''
     },
-    cancelCheckStatus (row, index) {
-      this.formData.checkStatus = this.currentStatus
-      this.showCheckStatusModal = false
-      this.$Message.info('取消审核')
+    // 取消提交审核
+    cancelCheck () {
+      this.showCheckResultModal = false
+      this.$Message.info('您已取消审核')
+      this.checkResult = ''
+      this.checkMsg = ''
     },
     search () {
       this.getParallelTestDataList()
